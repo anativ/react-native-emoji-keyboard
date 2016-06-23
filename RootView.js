9a46cc464a3d84d44b5fa830b175a80e8c0add7f
Ori Arditi <ori.rdt@gmail.com>
2016-06-23 16:28:57 +0300

    emoji button

--- RootView.js
+++ RootView.js
@@ -10,29 +10,62 @@ import {
   StyleSheet,
   Text,
   View,
-  TextInput
+  TouchableOpacity,
+  TextInput,
+  Keyboard,
+  Dimensions
 } from 'react-native';
 
 import TabbedView from './TabbedView';
+import dismissKeyboard from 'react-native-dismiss-keyboard';
 
 export default class RootView extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      text: ''
+      text: '',
+      emoji: false,
+      visibleHeight: Dimensions.get('window').height
     }
   }
   render() {
     return (
       <View style={styles.container}>
+        <View style={{flex:1, flexDirection: 'row', alignItems:'center', paddingTop: 20}}>
+        <TouchableOpacity onPress={this.toggleEmoji.bind(this)}>
+          <Text style={{width: 50, fontSize: 30, textAlign:'center'}}>😀</Text>
+        </TouchableOpacity>
         <TextInput
-          style={{height: 140, borderColor: 'gray', borderWidth: 1}}
+          style={{height: this.state.visibleHeight, borderColor: 'gray', borderWidth: 1,flex:1}}
           onChangeText={(text) => this.setState({text})}
           value={this.state.text}
+          multiline={true}
         />
-        <TabbedView onPress={this.onPress.bind(this)}/>
+        </View>
+        {this.state.emoji && <TabbedView onPress={this.onPress.bind(this)}/>}
       </View>
     );
+  }
+
+  toggleEmoji() {
+    const oldEmoji = this.state.emoji;
+    dismissKeyboard();
+    this.setState({emoji: !oldEmoji});
+  }
+
+  componentWillMount () {
+    Keyboard.addListener('keyboardWillShow', this.keyboardWillShow.bind(this))
+    Keyboard.addListener('keyboardWillHide', this.keyboardWillHide.bind(this))
+  }
+
+  keyboardWillShow (e) {
+    let newSize = Dimensions.get('window').height - e.endCoordinates.height
+    this.setState({visibleHeight: newSize})
+    this.setState({emoji: false});
+  }
+
+  keyboardWillHide (e) {
+    this.setState({visibleHeight: Dimensions.get('window').height})
   }
 
   onPress(x) {
